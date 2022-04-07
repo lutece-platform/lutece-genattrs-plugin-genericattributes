0d1c04dd1496905441d288af62c7face3ea271cd
rdeniel <renaud.deniel@outlook.com>
2022-04-07 17:59:48 +0200

    GENERICATT-132 add new field to entry type text
    GENERICATT-132 add new field to entry type text
    
    GENERICATT-132 add new field to entry type text
    
    GENERICATT-132 add new attribute to entry type text
    
    GENERICATT-132 update POM to use new Lutece Core's FileImagePublicService class
    
    GENERICATT-132 update sql script init
    
    GENERICATT-132 add sql script upgrade
    
    GENERICATT-132 add illustration image field
    
    GENERICATT-132 add illustration image field
    
    GENERICATT-132 add illustration image field
    
    add new field to entry type text, checkbox and radios
    
    GENERICATT-132 add new field to entry type text, checkbox and radios
    
    GENERICATT-132 add new field to entry type text
    
    GENERICATT-132 add new field to entry type text

--- src/java/fr/paris/lutece/plugins/genericattributes/service/ResponseImageResourceProvider.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/service/ResponseImageResourceProvider.java
@@ -99,4 +99,5 @@ public class ResponseImageResourceProvider implements ImageResourceProvider
 
         return urlItem.getUrl( );
     }
+
 }
--- src/java/fr/paris/lutece/plugins/genericattributes/service/entrytype/AbstractEntryTypeRadioButton.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/service/entrytype/AbstractEntryTypeRadioButton.java
@@ -39,11 +39,16 @@ import fr.paris.lutece.plugins.genericattributes.business.GenericAttributeError;
 import fr.paris.lutece.plugins.genericattributes.business.MandatoryError;
 import fr.paris.lutece.plugins.genericattributes.business.Response;
 import fr.paris.lutece.plugins.genericattributes.util.GenericAttributesUtils;
+import fr.paris.lutece.portal.service.fileimage.FileImagePublicService;
 import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.image.ImageResourceManager;
 import fr.paris.lutece.portal.service.message.AdminMessage;
 import fr.paris.lutece.portal.service.message.AdminMessageService;
+import fr.paris.lutece.portal.service.util.AppException;
 import fr.paris.lutece.portal.service.util.AppLogService;
+import fr.paris.lutece.portal.web.upload.MultipartHttpServletRequest;
 
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
@@ -56,6 +61,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 public abstract class AbstractEntryTypeRadioButton extends AbstractEntryTypeChoice
 {
+	private static final String MESSAGE_ERROR_FILE_IMAGE = "Error importing file.";
     /**
      * {@inheritDoc}
      */
@@ -71,6 +77,8 @@ public abstract class AbstractEntryTypeRadioButton extends AbstractEntryTypeChoi
         String strFieldInLine = request.getParameter( PARAMETER_FIELD_IN_LINE );
         String strCSSClass = request.getParameter( PARAMETER_CSS_CLASS );
         String strOnlyDisplayInBack = request.getParameter( PARAMETER_ONLY_DISPLAY_IN_BACK );
+        MultipartHttpServletRequest multipartRequest = ( MultipartHttpServletRequest ) request;
+        FileItem imageFileItem = multipartRequest.getFile( PARAMETER_ILLUSTRATION_IMAGE );
 
         int nFieldInLine = -1;
 
@@ -96,6 +104,19 @@ public abstract class AbstractEntryTypeRadioButton extends AbstractEntryTypeChoi
             return AdminMessageService.getMessageUrl( request, strFieldError, ERROR_FIELD_REF_LIST, AdminMessage.TYPE_STOP );
         }
 
+        if ( imageFileItem != null && imageFileItem.getSize( ) > 0 )
+        {
+            try
+            {
+                String strFileStoreKey = ImageResourceManager.addImageResource( FileImagePublicService.IMAGE_RESOURCE_TYPE_ID, imageFileItem);
+                GenericAttributesUtils.createOrUpdateField( entry, FIELD_ILLUSTRATION_IMAGE, null, strFileStoreKey );
+            }
+            catch ( Exception e ) 
+            {
+            	AppLogService.error( MESSAGE_ERROR_FILE_IMAGE, e );
+                throw new AppException( MESSAGE_ERROR_FILE_IMAGE, e );
+            }
+        }
         entry.setCode( strCode );
         entry.setTitle( strTitle );
         entry.setHelpMessage( strHelpMessage );
