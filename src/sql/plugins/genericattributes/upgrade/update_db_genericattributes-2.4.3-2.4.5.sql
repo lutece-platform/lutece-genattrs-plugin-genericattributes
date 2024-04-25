4fb0655d1dd09790374874fb68ea84f30331c08b
obfromevi <160587605+obfromevi@users.noreply.github.com>
2024-04-25 15:57:34 +0200

    LUT-26975 : use of IFileStoreServiceProvider implementation to store files contained in reponses of Forms

--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.3-2.4.5.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.3-2.4.5.sql
@@ -1,3 +1,8 @@
 INSERT INTO genatt_field (id_entry, code, VALUE, title)
 	SELECT e.id_entry, 'disabled', 'false', ''
 	from genatt_entry e ;
+
+UPDATE core_file f SET f.origin = 'defaultDatabaseFileStoreProvider'
+WHERE f.origin is null
+AND exists (SELECT 1 FROM genatt_response r WHERE r.id_file = f.id_file)
+;
