1b8eb487039ca372cbe056e84d33b9a742623f66
atos-mohamed-b <126660650+atos-mohamed-b@users.noreply.github.com>
2024-05-31 17:06:53 +0200

    LUT-25549: Updated the Table 'genatt_entry_type' to have an auto incremented ID

--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.3-2.4.5.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.3-2.4.5.sql
@@ -6,3 +6,5 @@ UPDATE core_file f SET f.origin = 'defaultDatabaseFileStoreProvider'
 WHERE f.origin is null
 AND exists (SELECT 1 FROM genatt_response r WHERE r.id_file = f.id_file)
 ;
+
+ALTER TABLE genatt_entry_type MODIFY COLUMN id_type int AUTO_INCREMENT NOT NULL;
