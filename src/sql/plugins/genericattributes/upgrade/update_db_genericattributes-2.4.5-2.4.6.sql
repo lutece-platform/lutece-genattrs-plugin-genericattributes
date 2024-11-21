b6e3a8aa40edc355ab1e194f8749fcad6cf3c23b
chedli <chedly.hafaiedh@gmail.com>
2024-11-21 09:14:11 +0100

    LUT-29000 : script to upgrade from 2.4.5 to 2.4.6 to fix default_today_date integration

--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.5-2.4.6.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.5-2.4.6.sql
@@ -1,2 +1,9 @@
 ALTER TABLE genatt_response CHANGE COLUMN id_file file_key VARCHAR(255);
 ALTER TABLE genatt_response ADD COLUMN file_store VARCHAR(255) DEFAULT NULL AFTER file_key;
+
+INSERT INTO genatt_field ( id_entry, title, code, VALUE, default_value )
+SELECT e.id_entry, null, 'default_today_date', 'false', 0
+FROM genatt_entry e
+         INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE e.resource_type = 'FORMS_FORM'
+  AND t.class_name = 'forms.entryTypeDate';
