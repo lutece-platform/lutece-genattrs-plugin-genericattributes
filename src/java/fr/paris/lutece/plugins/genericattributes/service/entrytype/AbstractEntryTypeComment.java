4d7199f32209fb08eab0d383c4f56a93519b4fa4
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-09-19 15:37:10 +0200

    LUT-30559 : Prevent void file added to comment entry

--- src/java/fr/paris/lutece/plugins/genericattributes/service/entrytype/AbstractEntryTypeComment.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/service/entrytype/AbstractEntryTypeComment.java
@@ -103,7 +103,7 @@ public abstract class AbstractEntryTypeComment extends EntryTypeService
             MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
             FileItem fileItem = multipartRequest.getFile( PARAMETER_FILE );
 
-            if ( fileItem != null && fileItem.get( ) != null )
+            if ( fileItem != null && fileItem.get( ) != null && StringUtils.isNotBlank( fileItem.getName( ) ) )
             {
                 removeOldFiles( entry );
 
