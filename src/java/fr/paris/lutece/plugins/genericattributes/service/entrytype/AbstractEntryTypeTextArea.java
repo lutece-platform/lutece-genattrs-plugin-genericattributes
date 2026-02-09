e8a56a5bf3b0022b5c96cb24a47cf0066a9f6dda
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2026-02-09 16:26:42 +0100

    LUT-31559 : Short answer question, with input confirmation enabled: if the confirmation field title is empty, a first message is displayed then an error is displayed (ERR_CACHE_MISS page).

--- src/java/fr/paris/lutece/plugins/genericattributes/service/entrytype/AbstractEntryTypeTextArea.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/service/entrytype/AbstractEntryTypeTextArea.java
@@ -68,6 +68,15 @@ public abstract class AbstractEntryTypeTextArea extends EntryTypeService
     @Override
     public String getRequestData( Entry entry, HttpServletRequest request, Locale locale )
     {
+    	return getRequestData( entry, request, locale, null );
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getRequestData( Entry entry, HttpServletRequest request, Locale locale, String errorReturnUrl )
+    {
         initCommonRequestData( entry, request );
         String strTitle = request.getParameter( PARAMETER_TITLE );
         String strCode = request.getParameter( PARAMETER_ENTRY_CODE );
@@ -112,7 +121,11 @@ public abstract class AbstractEntryTypeTextArea extends EntryTypeService
             Object [ ] tabRequiredFields = {
                     I18nService.getLocalizedString( strFieldError, locale )
             };
-
+            
+            if ( StringUtils.isNotBlank( errorReturnUrl ) )
+            {
+            	return AdminMessageService.getMessageUrl( request, MESSAGE_MANDATORY_FIELD, tabRequiredFields, errorReturnUrl, AdminMessage.TYPE_STOP );
+            }
             return AdminMessageService.getMessageUrl( request, MESSAGE_MANDATORY_FIELD, tabRequiredFields, AdminMessage.TYPE_STOP );
         }
 
@@ -154,7 +167,11 @@ public abstract class AbstractEntryTypeTextArea extends EntryTypeService
             Object [ ] tabRequiredFields = {
                     I18nService.getLocalizedString( strFieldError, locale )
             };
-
+            
+            if ( StringUtils.isNotBlank( errorReturnUrl ) )
+            {
+            	return AdminMessageService.getMessageUrl( request, MESSAGE_NUMERIC_FIELD, tabRequiredFields, errorReturnUrl, AdminMessage.TYPE_STOP );
+            }
             return AdminMessageService.getMessageUrl( request, MESSAGE_NUMERIC_FIELD, tabRequiredFields, AdminMessage.TYPE_STOP );
         }
 
