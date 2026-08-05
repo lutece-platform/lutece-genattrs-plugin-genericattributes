c9d8cadab7e502476b57721329107abe8122ae9c
Antoine Fuzier <afuzier@yupiik.com>
2026-08-05 10:39:03 +0200

    LUT-28872 : Implement removeSessionFiles for destroyed session

--- src/java/fr/paris/lutece/plugins/genericattributes/service/upload/AbstractGenAttUploadHandler.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/service/upload/AbstractGenAttUploadHandler.java
@@ -33,6 +33,7 @@
  */
 package fr.paris.lutece.plugins.genericattributes.service.upload;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -42,6 +43,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import fr.paris.lutece.portal.service.util.AppLogService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
@@ -176,15 +178,42 @@ public abstract class AbstractGenAttUploadHandler extends AbstractAsynchronousUp
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void removeSessionFiles( HttpSession session )
     {
         String sessionId = (String) session.getAttribute( PARAM_CUSTOM_SESSION_ID );
-        if ( sessionId != null )
+        if( StringUtils.isBlank( sessionId) )
         {
-            _mapAsynchronousUpload.remove( sessionId );
+            return;
         }
 
+        Map<String, List<MultipartItem>> mapFileItemsSession = _mapAsynchronousUpload.remove( sessionId );
+
+        if ( mapFileItemsSession != null )
+        {
+            for ( List<MultipartItem> fileItems : mapFileItemsSession.values( ) )
+            {
+                deleteFiles( fileItems );
+            }
+        }
+    }
+
+    private void deleteFiles( List<MultipartItem> fileItems )
+    {
+        for ( MultipartItem fileItem : fileItems )
+        {
+            try
+            {
+                fileItem.delete( );
+            }
+            catch ( IOException e )
+            {
+                AppLogService.error( "Unable to delete uploaded file " + fileItem.getName( ), e );
+            }
+        }
     }
 
     /**
