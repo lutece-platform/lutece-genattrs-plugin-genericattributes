3c7e644967bb8e472c240fb05d50fb7b26235803
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2026-08-26 14:50:48 +0200

    LUT-33259 : Own the generic genatt schema migrations duplicated in dependent plugins
    update_db_genericattributes-1.3.2-1.3.3.sql and -1.3.3-2.0.0.sql had every
    statement commented out ("To use on an empty database only"), the migration
    being left to dependent plugins (forms, appointment, announce...) which each
    shipped a copy of it. They now carry the migration themselves :
    - 1.3.2-1.3.3 : generic field-code data recovery keyed on the entry-type kind
      (class_name LIKE '%.entryTypeX'), only_display_in_back, id_field
      AUTO_INCREMENT, used_in_correct_form_response recovery, drop of
      is_shown_in_completeness
    - 1.3.3-2.0.0 : array_row/array_column, confirm_field, width/height/max_size
      recovery, then the column drops, id_entry AUTO_INCREMENT
    
    A sql-check precondition on the removed column makes them MARK_RAN on
    databases already migrated by a dependent plugin. Databases that recorded
    these changesets while they were empty are not expected to re-parse them
    (excluded by version) ; should a checksum mismatch ever show up, it will be
    handled case by case.
    
    Port to the core7 line (genericattributes 2.4.x, liquibase 4.31.1): the two
    files are identical on both lines, clean cherry-pick. On core7 the dependent
    plugins keep their copies of this migration (forms LUT-33260, appointment
    LUT-33258 core7 ports); both sets have destination versions below the first
    liquibase-enabled release of their plugin and are never included on a site that
    runs liquibase.

--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-1.3.2-1.3.3.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-1.3.2-1.3.3.sql
@@ -1,11 +1,57 @@
 -- liquibase formatted sql
 -- changeset genericattributes:update_db_genericattributes-1.3.2-1.3.3.sql
 -- preconditions onFail:MARK_RAN onError:WARN
--- To use on an empty database only
--- for a full updates, see plugins depending on genatt (forms, ticketing...)
-
---ALTER TABLE genatt_entry CHANGE COLUMN is_shown_in_completeness used_in_correct_form_response SMALLINT DEFAULT '0';
-
---ALTER TABLE genatt_field MODIFY id_field INT AUTO_INCREMENT;
-
---ALTER TABLE genatt_entry DROP COLUMN used_in_correct_form_response;
+-- precondition-sql-check expectedResult:1 SELECT COUNT(*) FROM information_schema.columns WHERE LOWER(table_name) = 'genatt_entry' AND LOWER(column_name) = 'is_shown_in_completeness'
+-- LUT-33259 : 1.3.3 field-code model : generic data recovery keyed on the entry-type kind, then the DDL formerly commented out.
+-- Formerly "To use on an empty database only" with every statement commented out, the migration being left to
+-- dependent plugins (forms, appointment, announce...) which each shipped a copy of it. The precondition makes it
+-- MARK_RAN on databases already migrated by a dependent plugin.
+UPDATE genatt_field f SET f.code = 'default_date_value'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeDate' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = f.title
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeGeolocation' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.title = NULL
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeGeolocation' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.value = f.title
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeNumbering' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'prefix'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeNumbering' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.title = NULL
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeNumbering' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = f.title
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE (t.class_name LIKE '%.entryTypeFile' OR t.class_name LIKE '%.entryTypeImage' OR t.class_name LIKE '%.entryTypeAutomaticFileReading') AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'file_config'
+WHERE f.code IS NULL AND f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE (t.class_name LIKE '%.entryTypeFile' OR t.class_name LIKE '%.entryTypeImage' OR t.class_name LIKE '%.entryTypeAutomaticFileReading') AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.title = NULL
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE (t.class_name LIKE '%.entryTypeFile' OR t.class_name LIKE '%.entryTypeImage' OR t.class_name LIKE '%.entryTypeAutomaticFileReading') AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'answer_choice'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE (t.class_name LIKE '%.entryTypeSelect' OR t.class_name LIKE '%.entryTypeRadioButton' OR t.class_name LIKE '%.entryTypeCheckBox') AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'array_cell'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeArray' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'text_config'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE (t.class_name LIKE '%.entryTypeText' OR t.class_name LIKE '%.entryTypeTextArea' OR t.class_name LIKE '%.entryTypePhone') AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'attribute_name'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeSession' AND e.id_entry = f.id_entry);
+UPDATE genatt_field f SET f.code = 'user_config'
+WHERE f.id_entry IN (SELECT e.id_entry FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeMyLuteceUser' AND e.id_entry = f.id_entry);
+UPDATE genatt_entry e SET e.is_only_display_back = '1'
+WHERE e.id_entry IN (SELECT f.id_entry FROM genatt_field f WHERE f.code = 'only_display_in_back' AND f.value = '1');
+DELETE FROM genatt_field WHERE code = 'only_display_in_back';
+ALTER TABLE genatt_field MODIFY id_field INT AUTO_INCREMENT;
+INSERT INTO genatt_field (id_entry, code, value)
+    SELECT id_entry, 'used_in_correct_form_response', CASE is_shown_in_completeness WHEN 1 THEN 'true' ELSE 'false' END FROM genatt_entry;
+ALTER TABLE genatt_entry DROP COLUMN is_shown_in_completeness;
--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-1.3.3-2.0.0.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-1.3.3-2.0.0.sql
@@ -1,25 +1,36 @@
 -- liquibase formatted sql
 -- changeset genericattributes:update_db_genericattributes-1.3.3-2.0.0.sql
 -- preconditions onFail:MARK_RAN onError:WARN
--- To use on an empty database only
--- for a full updates, see plugins depending on genatt (forms, ticketing...)
-
--- ALTER TABLE genatt_entry DROP COLUMN num_row;
--- ALTER TABLE genatt_entry DROP COLUMN num_column;
-
--- ALTER TABLE genatt_entry DROP COLUMN confirm_field;
--- ALTER TABLE genatt_entry DROP COLUMN confirm_field_title
-
--- ALTER TABLE genatt_field DROP COLUMN width;
-
--- ALTER TABLE genatt_field DROP COLUMN height;
-
--- ALTER TABLE genatt_field DROP COLUMN max_size_enter;
-
--- ALTER TABLE genatt_entry DROP COLUMN map_provider;
-
--- ALTER TABLE genatt_field DROP COLUMN image_type;
-
--- ALTER TABLE genatt_entry DROP COLUMN is_role_associated;
--- ALTER TABLE genatt_field DROP COLUMN role_key;
--- ALTER TABLE genatt_entry modify COLUMN id_entry int AUTO_INCREMENT NOT NULL;
+-- precondition-sql-check expectedResult:1 SELECT COUNT(*) FROM information_schema.columns WHERE LOWER(table_name) = 'genatt_entry' AND LOWER(column_name) = 'num_row'
+-- LUT-33259 : 2.0.0 fields model : array/confirm_field/width/height/max_size recovery, then the column drops formerly commented out.
+-- Formerly "To use on an empty database only" with every statement commented out, the migration being left to
+-- dependent plugins (forms, appointment, announce...) which each shipped a copy of it. The precondition makes it
+-- MARK_RAN on databases already migrated by a dependent plugin.
+INSERT INTO genatt_field (id_entry, code, value)
+    SELECT id_entry, 'array_row', num_row FROM genatt_entry WHERE num_row > 0;
+INSERT INTO genatt_field (id_entry, code, value)
+    SELECT id_entry, 'array_column', num_column FROM genatt_entry WHERE num_column > 0;
+ALTER TABLE genatt_entry DROP COLUMN num_row;
+ALTER TABLE genatt_entry DROP COLUMN num_column;
+INSERT INTO genatt_field (id_entry, code, value, title)
+    SELECT e.id_entry, 'confirm_field', CASE e.confirm_field WHEN 1 THEN 'true' ELSE 'false' END, e.confirm_field_title
+    FROM genatt_entry e INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+    WHERE t.class_name LIKE '%.entryTypeText';
+ALTER TABLE genatt_entry DROP COLUMN confirm_field;
+ALTER TABLE genatt_entry DROP COLUMN confirm_field_title;
+INSERT INTO genatt_field (id_entry, code, value)
+    SELECT id_entry, 'width', width FROM genatt_field WHERE width > 0 AND code NOT IN ('file_config', 'user_config');
+INSERT INTO genatt_field (id_entry, code, value)
+    SELECT id_entry, 'height', height FROM genatt_field WHERE height > 0;
+INSERT INTO genatt_field (id_entry, code, value)
+    SELECT id_entry, 'max_size', max_size_enter FROM genatt_field WHERE max_size_enter IS NOT NULL AND max_size_enter != 0;
+DELETE FROM genatt_field WHERE code = 'file_config';
+DELETE FROM genatt_field WHERE code = 'user_config';
+ALTER TABLE genatt_field DROP COLUMN width;
+ALTER TABLE genatt_field DROP COLUMN height;
+ALTER TABLE genatt_field DROP COLUMN max_size_enter;
+ALTER TABLE genatt_entry DROP COLUMN map_provider;
+ALTER TABLE genatt_field DROP COLUMN image_type;
+ALTER TABLE genatt_entry DROP COLUMN is_role_associated;
+ALTER TABLE genatt_field DROP COLUMN role_key;
+ALTER TABLE genatt_entry MODIFY COLUMN id_entry INT AUTO_INCREMENT NOT NULL;
